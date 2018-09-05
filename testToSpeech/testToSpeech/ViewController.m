2ba069953c387513e229caddc1bd81755fdcd27f
Jany <3236033215@qq.com>
2018-09-05 18:24:38 +0800

    测试

--- testToSpeech/testToSpeech/ViewController.m
+++ testToSpeech/testToSpeech/ViewController.m
@@ -25,7 +25,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    //dasdasdasdasdsa
     speechSynthesizer = [[AVSpeechSynthesizer alloc] init];
     speechSynthesizer.delegate = self;
     
